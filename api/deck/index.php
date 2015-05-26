edb0af072abfc2e750196be02f3b7012e2d449ef
Willem Labu <willem@flintntinder.com>
2015-05-26 18:58:00 +0200

    C:/Program Files (x86)/Git/deck/ endpoint updated

--- api/deck/index.php
+++ api/deck/index.php
@@ -1,41 +1,71 @@
 <?php
 
-include "../includes/functions.php";
-/*$cards = [
-    'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13',
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13',
-    'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'd12', 'd13',
-    's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13'
-];*/
+    include "../includes/functions.php";
 
-    $id = $_GET['id'];
+    // Get the id if it exists.
+    $id = isset($_GET['id']) ? $_GET['id'] : false;
 
-    $sql = "SELECT * FROM decks WHERE id = $id";
-    //print_r($sql);
+    // Handle the case that the user did not
+    //  supply an ID at all.
+    if (!$id) {
+
+        $resp = new Response();
+
+        $resp->code = 404;
+        $resp->status = "error";
+        $resp->body = "Please supply a valid ID.";
+
+        die (json_encode($resp));
+
+    }
+
+    // Here, at least, we have some form of an ID
+
+    // Create some SQL
+    $sql = "SELECT * FROM decks WHERE guid = :id";
+    
     // Let's connect to the database
     $db = dbConnect();
+    
     // Then, prepare the statement to run..
     $statement = $db->prepare($sql);
+
     // execute the prepared statement
-    $statement->execute();
+    $statement->execute(array(':id' => $id));
+
     // Go get the data.
-    $data = $statement->fetchall(PDO::FETCH_ASSOC);
-     //echo '<pre>', print_r($data) ,'</pre>';
+
+    // This returns an array of all rows that match
+    //  even if only one row does.
+    // $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // This returns a proper 1 level array
+    $data = $statement->fetch(PDO::FETCH_ASSOC); 
+
     // Close the connection!
     $db = $statement = null;
 
-    // Make the deck from string into array to shuffle again
-     $deck = explode(',', $data[0]['deck']);
-     //shuffle($deck);
-    //  echo '<pre>', print_r($deck), '</pre>';
-    /*
-    // split the string into elements to read the suits.
-    foreach ($deck as $card) {
 
-        $split = str_split($card,1);
+    // Let's see if anything was returned.
+    if (!$data) {
 
-        echo '<pre>', print_r($split), '</pre>';
-    }*/
+        $resp = new Response();
+
+        $resp->code = 404;
+        $resp->status = "error";
+        $resp->body = "NO deck was found with that ID.";
+
+        die (json_encode($resp));
+
+    }
+
+
+    // Here we actually have a proper ID! YAY :{D
+
+
+    // Re-array-ify the deck we saved as a string
+    $deck = explode(',', $data['deck']);
+
     // Nice response
     $response = new Response();
 
@@ -44,12 +74,11 @@ include "../includes/functions.php";
         'id' => $id,
         'deck' => $deck
     );
+
     $response->code = 200;
     $response->status = "success";
     $response->body = $body;
 
     header("Content-Type: application/json");
     echo json_encode($response);
-
-
 
